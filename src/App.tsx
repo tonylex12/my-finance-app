7095506e541b666a304a0af3634c138cce194a0c
Tony HUerto <tonylex12@gmail.com>
2024-12-31 12:44:44 -0500

    ADD: routes with layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
+import { Routes, Route } from "react-router-dom";
 import "./globals.css";
+import SignInForm from "./_auth/forms/SignInForm";
+import { Home } from "./_root/pages";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hola Finance App</h1>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* Pulic routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+
+        {/* Private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 export default App;
