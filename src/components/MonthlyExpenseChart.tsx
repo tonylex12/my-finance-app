602cdb269e95628ac94acf96236547d8fcbd706a
Tony HUerto <tonylex12@gmail.com>
2025-01-15 02:03:22 -0500

    FIX: console log deleted on MonthlyExpenseChart component

--- src/components/MonthlyExpenseChart.tsx
+++ src/components/MonthlyExpenseChart.tsx
@@ -74,8 +74,6 @@ const MonthlyExpenseChart = ({
     return weekData;
   });
 
-  console.log("Chart Data:", chartData);
-
   const dayNames = ["LU", "MA", "MI", "JU", "VI", "SÁ", "DO"];
 
   return (
